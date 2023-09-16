b90fffaaf9cb520a65e7c8cbed73a010051290aa
caizelun <caizelun@bytedance.com>
2023-09-16 19:55:47 +0800

    feat: add test server

---
+++ service/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+
+app.use(express.json())
+
+app.post('/model', (req, res) => {
+  const { prompt, image } = req.body
+
+  // 进行处理逻辑，如果只是原样返回的话，可以直接将数据返回
+  const response = { message: 'Response Successfully', status: '1', data: { result: 'description' } }
+
+  res.json(response)
+})
+
+app.listen(3002, () => { })
